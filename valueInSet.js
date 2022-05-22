6c3fdc2fad9c1d33e8e7e195bbafe2ff6ca44697
AbdulKhader <abdul.khader5166@gmail.com>
2022-05-22 09:38:07 +0530

    To check a particular value is present in the set

---
+++ valueInSet.js
@@ -0,0 +1,8 @@
+//A function that checks wether the value is present in set
+
+// My solution, Preffered solution
+function myFunction(set, val) {
+    return set.has(val);
+ }
+//myFunction(new Set([1, 2, 3]), 2)
+//expected True
