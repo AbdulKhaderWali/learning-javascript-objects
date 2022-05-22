99a2d9703a1fb47422c9ddb34c8ae73323e8ee7a
AbdulKhader <abdul.khader5166@gmail.com>
2022-05-22 09:49:08 +0530

    To return last 3 elements of array

---
+++ lastThree.js
@@ -0,0 +1,11 @@
+// A function that extracts last 3 elements of array
+// returns that last 3 elements in form of array
+
+
+// My solution, Preferred solution
+function myFunction(a){
+    return a.slice(-3);
+} 
+
+// myFunction([99,1,1])
+// [99,1,1]
