6e6f629c988c36b195b70b0540597875ffa5b0dd
AbdulKhader <abdul.khader5166@gmail.com>
2022-06-11 08:09:16 +0530

    Rounding off a decimal number upto two places

---
+++ roundDecNum.js
@@ -0,0 +1,8 @@
+// Write a function that takes a number (a) as argument
+// Round a to the 2nd digit after the comma
+// Return the rounded number
+
+//My solution or Preferred solution
+function myFunction(a) {
+    return Number(a.toFixed(2));
+  }
